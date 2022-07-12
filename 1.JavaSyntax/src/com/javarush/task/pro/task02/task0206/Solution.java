699776b03a37c8e339b4f90115d6c548d0f4562c
Evgeny <gnusover@gmail.com>
2022-07-12 15:32:17 +0300

    add solution from JavaSyntax task 0206

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -3,7 +3,10 @@ package com.javarush.task.pro.task02.task0206;
 public class Solution {
 
     public static void main(String[] args) {
-        String word = "1", phrase = "2", line = "3", text = "4";
+        String word = "1";
+        String phrase = "2";
+        String line = "3";
+        String text = "4";
 
 
     }
